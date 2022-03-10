94cd009d3bd6fa28e22e49d8c4bb88731aeba0ed
Aaron Stolle <aaronstolle@gmail.com>
2022-03-10 10:54:19 -0600

    Print an appropriate string based upon the boolean results from IsWithinRange

--- ConditionalLoopMethodsDemo/Program.cs
+++ ConditionalLoopMethodsDemo/Program.cs
@@ -16,14 +16,16 @@ namespace ConditionalLoopMethodDemo
             Console.WriteLine("Enter a range: ");
             int range = int.Parse(Console.ReadLine());
 
+            string results = IsWithinRange(startingNum, targetNum, range) ? "It's within range." : "It's not within range.";
+
             Console.WriteLine($"Given a starting number of: {startingNum} and a target number: {targetNum}. " +
-                $"It's {IsWithinRange(startingNum, targetNum, range)}");
+                $"{results}");
         }
 
         //access modifier - static(no not) - return type - name(parms in here)
         private static bool IsWithinRange(int num, int target, int range)
         {
-            return (num - target) <= range;
+            return Math.Abs(num - target) <= range;
         }
     }
 }
