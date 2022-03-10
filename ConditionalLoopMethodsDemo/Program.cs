3a46bd607291e98e363714c4f1f9ac3b850c4cc4
Aaron Stolle <aaronstolle@gmail.com>
2022-03-10 10:32:17 -0600

    Output a string that states if two numbers are within a range of each other

--- ConditionalLoopMethodsDemo/Program.cs
+++ ConditionalLoopMethodsDemo/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConditionalLoopMethodDemo
+{
+    class Mainclass
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Enter an initial number: ");
+            int startingNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a target number: ");
+            int targetNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a range: ");
+            int range = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Given a starting number of: {startingNum} and a target number: {targetNum}. " +
+                $"It's {IsWithinRange(startingNum, targetNum, range)}");
+        }
+
+        //access modifier - static(no not) - return type - name(parms in here)
+        private static bool IsWithinRange(int num, int target, int range)
+        {
+            return (num - target) <= range;
+        }
+    }
+}
